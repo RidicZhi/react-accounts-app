e60dfa0683e8e37ca54853a55762af617e3fcc9e
RidicZhi <ridiczhi@gmail.com>
2019-04-22 03:50:44 +1000

    Updates

---
+++ precache-manifest.fc54269bb6656af54354d9e4185fd663.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46364e9ef408c79a58e4",
+    "url": "/react-accounts-app/static/js/runtime~main.e8101d8e.js"
+  },
+  {
+    "revision": "13842ec86f203fe7e92d",
+    "url": "/react-accounts-app/static/js/main.cd6a6f17.chunk.js"
+  },
+  {
+    "revision": "b60bdcba33ced9d3df9f",
+    "url": "/react-accounts-app/static/js/2.c943308d.chunk.js"
+  },
+  {
+    "revision": "13842ec86f203fe7e92d",
+    "url": "/react-accounts-app/static/css/main.3baf28bb.chunk.css"
+  },
+  {
+    "revision": "c10d1bbb08f6185acf38f9a3b95300b4",
+    "url": "/react-accounts-app/index.html"
+  }
+];
