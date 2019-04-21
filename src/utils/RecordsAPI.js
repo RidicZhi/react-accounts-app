729ba032c908b7516b47a25e9f2c6989e5a2fcb3
RidicZhi <ridiczhi@gmail.com>
2019-04-22 03:56:32 +1000

    fix API setting

--- src/utils/RecordsAPI.js
+++ src/utils/RecordsAPI.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api =
   process.env.REACT_APP_RECORDS_API_URL ||
-  "https://5cb5bfa207f233001424d6c5.mockapi.io/api/v1/records";
+  "https://5cb5bfa207f233001424d6c5.mockapi.io";
 
 export const getRecords = () => 
   axios.get(`${api}/api/v1/records`);
